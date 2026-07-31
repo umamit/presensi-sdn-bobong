7be7c43c061dfb65e31fbbae55668d89b5d3d004
umamit <shot.ann09@gmail.com>
2026-07-31 19:06:03 +0900

    Update default school GPS geofence radius to strict 20 meters

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -21,7 +21,7 @@ export const INITIAL_SCHOOL_SETTINGS: SchoolSettings = {
   address: 'Area Presensi SD Negeri Bobong, Taliabu Barat, Maluku Utara',
   latitude: -1.955544,   // Koordinat titik tengah area presensi dari KML
   longitude: 124.384388, // Koordinat titik tengah area presensi dari KML
-  radiusMeters: 40,      // Toleransi radius absensi
+  radiusMeters: 20,      // Toleransi radius absensi (Strict 20 Meter)
   polygonCoords: [       // Polygon Geofence presisi dari Google Earth KML
     [-1.955389, 124.384231],
     [-1.955707, 124.384250],
