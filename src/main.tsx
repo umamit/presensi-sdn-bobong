8c044446f51fd09355971b4abd07ac88ea9a4f88
umamit <shot.ann09@gmail.com>
2026-07-31 16:55:46 +0900

    Explicitly register Service Worker and add manifest link tag for 100% PWABuilder detection

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { registerSW } from 'virtual:pwa-register';
 import { App } from './App';
 import './index.css';
+
+// Pendaftaran otomatis Service Worker untuk PWABuilder & PWA Installability
+registerSW({ immediate: true });
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
