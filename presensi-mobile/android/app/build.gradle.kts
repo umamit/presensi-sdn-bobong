c0d0405b8cff29b2f3f84d45f92ce343c435dbbc
umamit <shot.ann09@gmail.com>
2026-08-18 11:38:59 +0900

    fix: aktifkan core library desugaring untuk mendukung flutter_local_notifications

--- presensi-mobile/android/app/build.gradle.kts
+++ presensi-mobile/android/app/build.gradle.kts
@@ -12,6 +12,7 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+        isCoreLibraryDesugaringEnabled = true
     }
 
     defaultConfig {
@@ -42,4 +43,8 @@ kotlin {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
 }
